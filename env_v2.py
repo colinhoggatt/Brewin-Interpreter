b3439b5b9755e04a37b7f6993cc5012ff3d731cd
Colin Hoggatt <colinhoggatt@gmail.com>
2023-11-04 20:24:36 -0700

    Project 2 Submission

--- env_v2.py
+++ env_v2.py
@@ -5,6 +5,8 @@
 class EnvironmentManager:
     def __init__(self):
         self.environment = {}
+        # self.env_stack = []
+
 
     # Gets the data associated a variable name
     def get(self, symbol):
@@ -15,3 +17,60 @@ class EnvironmentManager:
     # Sets the data associated with a variable name
     def set(self, symbol, value):
         self.environment[symbol] = value
+
+
+
+#Thought Process:
+# We need to create an environment stack to hold the scope of the entire program and each of its blocks
+# For each block, we should push a new environment onto the environment stack and store its variables there
+# If we don't find a value or variable in the current scope, loop through the stack and check the next most recent scope
+class EnvironmentStack:
+    def __init__(self, initial):
+        self.stack = [initial]
+        
+    def push(self, environment): # push a new environment(scope) onto the stack
+        self.stack.append(environment)
+    
+    def pop(self): # remove the most recent scope
+        try:
+            self.stack.pop()
+        except:
+            raise IndexError
+        
+
+    def get(self, symbol): #get the value of the symbol, starting from the most recent scope
+        for scope in reversed(self.stack):
+            if symbol in scope.environment:
+                return scope.environment[symbol]
+        return None
+        raise NameError # how to raise an error if item is not in any scope??
+    
+    # need a global set so if variables aren't in current scope, set them in the next closest scope
+    def set(self, symbol, value):
+        #check if in current scope
+        if symbol in self.top().environment: # if the symbol is in current scope, set it
+            #self.environment.set(symbol, value)
+           # self.stack[-1].environment.set(symbol, value)
+           self.stack[-1].environment[symbol] = value
+        else:
+            found_flag = False
+            for scope in reversed(self.stack): #if not in current scope, iterate through the rest of the scopes
+                if symbol in scope.environment: # if it's in a different scope, set the value in that scope
+                    #self.scope.environment.set(symbol, value)
+                    scope.environment[symbol] = value
+                    found_flag = True
+                    break
+            if found_flag is False: # if not in any of the scopes, set it in the current scope
+                #self.stack[-1].environment.set(symbol, value)
+                self.stack[-1].environment[symbol] = value
+                
+                
+
+
+    def top(self): #get the current scope (IDK if we need this)
+        if len(self.stack) <= 0:
+            return None
+        else:
+            return self.stack[-1]
+        
+        
--- interpreterv2.py
+++ interpreterv2.py
@@ -1,4 +1,4 @@
-from env_v2 import EnvironmentManager
+from env_v2 import EnvironmentManager, EnvironmentStack
 from type_valuev1 import Type, Value, create_value, get_printable
 from intbase import InterpreterBase, ErrorType
 from brewparse import parse_program
@@ -16,6 +16,7 @@ class Interpreter(InterpreterBase):
         super().__init__(console_output, inp)
         self.trace_output = trace_output
         self.__setup_ops()
+        # self.is_returned = False
 
     # run a program that's provided in a string
     # usese the provided Parser found in brewparse.py to parse the program
@@ -23,92 +24,168 @@ class Interpreter(InterpreterBase):
     def run(self, program):
         ast = parse_program(program)
         self.__set_up_function_table(ast)
-        main_func = self.__get_func_by_name("main")
+        main_func = self.__get_func_by_name("main", 0)
         self.env = EnvironmentManager()
+        self.env_stack = EnvironmentStack(self.env)
         self.__run_statements(main_func.get("statements"))
 
     def __set_up_function_table(self, ast):
         self.func_name_to_ast = {}
         for func_def in ast.get("functions"):
-            self.func_name_to_ast[func_def.get("name")] = func_def
+            if func_def.get("name") in self.func_name_to_ast:
+                self.func_name_to_ast[func_def.get("name")].append(func_def)
+            else:
+                self.func_name_to_ast[func_def.get("name")] = [func_def]
 
-    def __get_func_by_name(self, name):
+    def __get_func_by_name(self, name, arg_count):
+        # print("dict", self.func_name_to_ast)
+        # print("name", name)
         if name not in self.func_name_to_ast:
             super().error(ErrorType.NAME_ERROR, f"Function {name} not found")
-        return self.func_name_to_ast[name]
+        else:
+            for func in self.func_name_to_ast[name]:
+                # print("arg count", len(func.get("args")))
+                if len(func.get("args")) == arg_count:
+                    return func
+                
+            super().error(ErrorType.NAME_ERROR, f"Function {name} has wrong number of args")
+
+        #return self.func_name_to_ast[name]
 
     def __run_statements(self, statements):
+      
         # all statements of a function are held in arg3 of the function AST node
+        return_val = None
         for statement in statements:
+            
             if self.trace_output:
-                print(statement)
+                print(statement)   
             if statement.elem_type == InterpreterBase.FCALL_DEF:
                 self.__call_func(statement)
             elif statement.elem_type == "=":
                 self.__assign(statement)
             elif statement.elem_type == InterpreterBase.IF_DEF:
-                self.__run_if(statement)
+                return_val =  self.__run_if(statement)
             elif statement.elem_type == InterpreterBase.WHILE_DEF:
-                self.__run_while(statement)
+                return_val =  self.__run_while(statement)
             elif statement.elem_type == InterpreterBase.RETURN_DEF:
-                self.__run_ret(statement)
+                return_val =  self.__run_ret(statement)
+                #break
 
-        return Interpreter.NIL_VALUE
+            if return_val != None:
+                return return_val
 
     def __run_if(self, statement):
+        #ADD SCOPING FOR IF
+        self.if_env = EnvironmentManager() # Create the environment for if
+        self.env_stack.push(self.if_env) # Push the environment/scope to the stack
+
+        return_if_else = None
         cond = self.__eval_expr(statement.get("condition"))
         if (cond.type() != Type.BOOL):
             super().error(ErrorType.TYPE_ERROR, f"Invalid if condition")
         if cond.value():
             st_list = statement.get("statements")
-            self.__run_statements(st_list)
+            return_if_else = self.__run_statements(st_list)
         else:
             # if no else statements, return None
-            if statement.get("else_statements") == None:
-                return
-            else:
-                else_list = statement.get("else_statements")
-                self.__run_statements(else_list)
-    
+            else_list = statement.get("else_statements") 
+            if else_list is not None:
+                return_if_else = self.__run_statements(else_list)
+        
+        #Make sure we have a return value to return (i.e. not assignment)
+        if return_if_else is not None:
+            self.env_stack.pop() # pop the scope from the stack
+            return return_if_else
+
+            #OLD CODE (doesn't work)
+            # if statement.get("else_statements") == None:
+            #     return 
+            # else:
+            #     else_list = statement.get("else_statements")
+            #     return self.__run_statements(else_list)
+        self.env_stack.pop()
     
     def __run_while(self, statement):
+        #ADD SCOPING
+
+        self.while_env = EnvironmentManager() # Create the environment for if
+        self.env_stack.push(self.while_env) # Push the environment/scope to the stack
+
+
+        return_val = None
         cond = self.__eval_expr(statement.get("condition"))
         if (cond.type() != Type.BOOL):
             super().error(ErrorType.TYPE_ERROR, f"Invalid while condition")
         while cond.value():
+            #print("while loop condition is TRUE")
             st_list = statement.get("statements")
-            self.__run_statements(st_list)
+            return_val = self.__run_statements(st_list)
+            if return_val != None:
+                self.env_stack.pop() # pop the scope from the stack
+                return return_val
             cond = self.__eval_expr(statement.get("condition"))
+        
+        self.env_stack.pop()
+            #return return_val
         
     def __run_ret(self, return_st):
         return_val = self.__eval_expr(return_st.get("expression"))
+        #print("Returning Val:", return_val.value())
         return return_val
+    
+        
 
     def __call_func(self, call_node):
+        # #Scoping initialization
+        # self.func_env = EnvironmentManager() # Create the environment for func calls
+        # self.env_stack.push(self.func_env) # Push the environment/scope to the stack
+        #TOO EARLY
+
         func_name = call_node.get("name")
         if func_name == "print":
             return self.__call_print(call_node)
         if func_name == "inputi" or func_name == "inputs":
             return self.__call_input(call_node)
+        
+        #Scoping initialization
+        self.func_env = EnvironmentManager() # Create the environment for func calls
+        self.env_stack.push(self.func_env) # Push the environment/scope to the stack
 
-        # add code here later to call other functions
-        function_called = self.__get_func_by_name(func_name)
-        args_list = function_called.get('args')
-        # bind arguments
-        for arg in args_list:
-            #set variables/arguments in a subdictionary for the function??
-            pass
-        func_statements = function_called.get('statements')
+        num_args = len(call_node.get("args"))
+        func_def = self.__get_func_by_name(func_name, num_args)
+        func_def_args =  func_def.get('args')
+        func_called_args = call_node.get('args')
 
-        if len(args_list) != len(call_node.get('args')):
-            super().error(ErrorType.NAME_ERROR, f"Invalid arguments")
-        return self.__run_statements(func_statements)
+        # if len(func_def_args) != len(func_called_args):
+        #     super().error(ErrorType.NAME_ERROR, f"Invalid argument length")
+        
+        for d_arg, c_arg in zip(func_def_args, func_called_args): # create a tuple to loop and access both the arguments in the function def and function call at the same time
+            def_arg = d_arg.get('name')
+            call_arg = self.__eval_expr(c_arg)
+            # print(def_arg)
+            # print(call_arg)
+            self.env_stack.top().set(def_arg, call_arg)
+        
+        func_statements = func_def.get('statements')
+        return_value = self.__run_statements(func_statements)
+
+
+        if return_value != None:
+            self.env_stack.pop()
+            return return_value
+        
+        self.env_stack.pop()
+        return Interpreter.NIL_VALUE
+
+
 
 
     def __call_print(self, call_ast):
         output = ""
         for arg in call_ast.get("args"):
             result = self.__eval_expr(arg)  # result is a Value object
+            #print("print result", result)
             output = output + get_printable(result)
         super().output(output)
         return Interpreter.NIL_VALUE
@@ -132,20 +209,23 @@ class Interpreter(InterpreterBase):
     def __assign(self, assign_ast):
         var_name = assign_ast.get("name")
         value_obj = self.__eval_expr(assign_ast.get("expression"))
-        self.env.set(var_name, value_obj)
+        self.env_stack.set(var_name, value_obj) #NEW TODAY
 
     def __eval_expr(self, expr_ast):
+        if expr_ast == None:
+            return Value(Type.NIL, None)
+        if expr_ast.elem_type == InterpreterBase.BOOL_DEF: # NEW
+            return Value(Type.BOOL, expr_ast.get("val"))
         if expr_ast.elem_type == InterpreterBase.INT_DEF:
             return Value(Type.INT, expr_ast.get("val"))
         if expr_ast.elem_type == InterpreterBase.NIL_DEF or expr_ast.elem_type == None:
-            return Value(Type.NIL, Interpreter.NIL_VALUE)
+            return Value(Type.NIL, None)
         if expr_ast.elem_type == InterpreterBase.STRING_DEF:
             return Value(Type.STRING, expr_ast.get("val"))
-        if expr_ast.elem_type == InterpreterBase.BOOL_DEF: # NEW
-            return Value(Type.BOOL, expr_ast.get("val"))
+        
         if expr_ast.elem_type == InterpreterBase.VAR_DEF:
             var_name = expr_ast.get("name")
-            val = self.env.get(var_name)
+            val = self.env_stack.get(var_name)
             if val is None:
                 super().error(ErrorType.NAME_ERROR, f"Variable {var_name} not found")
             return val
@@ -164,19 +244,25 @@ class Interpreter(InterpreterBase):
     def __eval_op(self, arith_ast):
         left_value_obj = self.__eval_expr(arith_ast.get("op1"))
         op_type = arith_ast.elem_type
+
         if op_type == "neg" or op_type == "!":
             # Handle incorrect types for negative and logical not operator
-            if op_type == "neg" and left_value_obj != Type.INT:
-                super().error(ErrorType.NAME_ERROR, f"Cannot take the negative of a non-integer value")
-            if op_type == "!" and left_value_obj != Type.BOOL:
-                super().error(ErrorType.NAME_ERROR, f"Cannot perform 'not' operation on a non-boolean value")
+            if op_type == "neg" and (left_value_obj.type() != Type.INT): #or isinstance(left_value_obj.value(), bool)): # NEW TODAY but I think this already is checked
+                super().error(ErrorType.TYPE_ERROR, f"Cannot take the negative of a non-integer value")
+            if op_type == "!" and left_value_obj.type() != Type.BOOL:
+                super().error(ErrorType.TYPE_ERROR, f"Cannot perform 'not' operation on a non-boolean value")
+            #Just return the left operand for unary operations
             f = self.op_to_lambda[left_value_obj.type()][arith_ast.elem_type]
             return f(left_value_obj)
 
         right_value_obj = self.__eval_expr(arith_ast.get("op2"))
 
+        # is and not equal comparison handling
         if arith_ast.elem_type == "==" or arith_ast.elem_type == "!=": 
             f = self.op_to_lambda[left_value_obj.type()][arith_ast.elem_type]
+            # print (left_value_obj.type())
+            # print (right_value_obj.type())
+            return f(left_value_obj, right_value_obj)
         if left_value_obj.type() != right_value_obj.type():
             super().error(
                 ErrorType.TYPE_ERROR,
@@ -213,6 +299,7 @@ class Interpreter(InterpreterBase):
         )
         self.op_to_lambda[Type.STRING]["=="] = lambda x, y: Value(
             Type.BOOL, x.value() == y.value()
+            if x.type() == y.type() else False
         )
         self.op_to_lambda[Type.STRING]["!="] = lambda x, y: Value(
             Type.BOOL, x.value() != y.value()
@@ -243,9 +330,13 @@ class Interpreter(InterpreterBase):
         )
         self.op_to_lambda[Type.INT]["=="] = lambda x, y: Value(
             Type.BOOL, x.value() == y.value()
+            #if isinstance(x, int) and isinstance(y, int) else False
+            if x.type() == y.type() else False
         )
         self.op_to_lambda[Type.INT]["!="] = lambda x, y: Value(
             Type.BOOL, x.value() != y.value()
+            #if isinstance(x, int) and isinstance(y, int) else True
+            if x.type() == y.type() else True
         )
         self.op_to_lambda[Type.BOOL]["!"] = lambda x: Value(
             Type.BOOL, not x.value()
@@ -265,11 +356,12 @@ class Interpreter(InterpreterBase):
         #NIL EDGE CASES
         self.op_to_lambda[Type.NIL]["=="] = lambda x, y: Value(
             Type.BOOL, x.value() == y.value()
-            if x.type() == y.type() else Value(Type.BOOL, False)
+            if x.type() == y.type() else False
+            #Got rid of this to passs test_nil.br and test_ret2.br
         )
         self.op_to_lambda[Type.NIL]["!="] = lambda x, y: Value(
             Type.BOOL, x.value() != y.value()
-            if x.type() == y.type() else Value(Type.BOOL, True)
+            if x.type() == y.type() else True
         )
         
 
@@ -281,37 +373,72 @@ class Interpreter(InterpreterBase):
   # all programs will be provided to your interpreter as a python string, 
   # just as shown here.
 
+# def main():
+#     program_source = """
+#         func foo() { 
+#             print("hello");
+#         }
+#         func bar() {
+#             return;
+#         }
+
+#         func main() {
+#             val = nil;
+#             if (foo() == val && bar() == nil) { print("this should print!"); }
+#         }
+
+#         """
+
+#     interpreter = Interpreter()
+#     interpreter.run(program_source)
+
+# if __name__ == "__main__":
+#     main()
+
+
+# def main():
+#     program_source = """
+#         func main() {
+#             print(foo(true) != nil);
+#             print(foo(false) == nil);
+#         }
+
+#         func foo(x) {
+#             print("hi");
+#             if (x) {
+#                 print("returning true");
+#                 return true;
+#             }
+#         }
+
+#         """
+
+#     interpreter = Interpreter()
+#     interpreter.run(program_source)
+
+# if __name__ == "__main__":
+#     main()
+
+
+
 def main():
     program_source = """
-        func foo(){
-            print("FOO");
-        }
-
+        
         func main() {
-            
-        a = 3;
-        if (a < 5){
-            print("if statement working");
-        }
-        else{
-            print("I love Matthew Tsega");
+            print (fib(4));
         }
 
-        g = 0;
-
-        while (g < 4){
-            print ("g = ", g);
-            g = g+1;
+        func fib(x){
+            if (x < 3){
+                
+                return 1;
+            }
+            else{
+                return fib(x-2) + fib(x-1);
+            }
         }
-
-        h = nil;
-        if (h == nil){
-            print("NIL WORKS");
-        }
-
-        foo();
-
-        }
+        
+        
         """
 
     interpreter = Interpreter()
@@ -319,6 +446,124 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# def main():
+#     program_source = """
+#         func foo() {
+#         i = 0;
+#         while (i < 3) {
+#             j = 0;
+#             while (j < 3) {
+#                 k = 0;
+#                 while (k < 3) {
+#                     if (i * j * k == 1) {
+#                         return ans;
+#                     } else {
+#                         ans = ans + 1;
+#                         k = k + 1;
+#                     }
+#                 }
+#                 j = j + 1;
+#             }
+#             i = i + 1;
+#         }
+#         }
+
+#         func main() {
+#         ans = 0;
+#         print(foo());
+#         print(ans);
+#         }
+#         """
+
+#     interpreter = Interpreter(trace_output=False)
+#     interpreter.run(program_source)
+
+# if __name__ == "__main__":
+#     main()
+
+# def main():
+#     program_source = """
+#         func foo() {
+#             return 5;
+#         }
+#         func main() {
+#             ans = 0;
+#             print(foo());
+#             print (ans);
+#         }
+#         """
+
+#     interpreter = Interpreter()
+#     interpreter.run(program_source)
+
+# if __name__ == "__main__":
+#     main()
+
+# def main():
+#     program_source = """
+#         func main() {
+#             ans = 0;
+#             i = 0;
+#             while (i < 3) {
+#                 print("in while");
+#                 if (i == 2){
+#                     return ans;
+#                 }
+#                 ans = ans + 1;
+#                 i = i + 1;
+#             }
+#             print("normal", ans);
+#         }
+#         """
+
+#     interpreter = Interpreter()
+#     interpreter.run(program_source)
+
+# if __name__ == "__main__":
+#     main()
+
+# def main():
+#     program_source = """
+#         func bar(x, y){
+#             z = x + y;
+#             print(z);
+#         }
+
+#         func main() {
+            
+#         a = 3;
+#         if (a < 5){
+#             print("if statement working");
+#         }
+#         else{
+#             print("I love Matthew Tsega");
+#         }
+
+#         g = 0;
+
+#         while (g < 4){
+#             print ("g = ", g);
+#             g = g+1;
+
+#             return 
+#         }
+
+#         h = nil;
+#         if (h == nil){
+#             print("NIL WORKS");
+#         }
+
+#         bar(9, 10);
+
+#         }
+#         """
+
+#     interpreter = Interpreter()
+#     interpreter.run(program_source)
+
+# if __name__ == "__main__":
+#     main()
 
 
 # def main():
